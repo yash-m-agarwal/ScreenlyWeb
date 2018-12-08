ef8756dc941c2b9f8ae987cacc547765cbf80276
yash-m-agarwal <yashmagarwal786@gmail.com>
2018-12-09 03:17:54 +0530

    Database implemented redirected

--- landingPage/landingPage/urls.py
+++ landingPage/landingPage/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
 	path('login/', include('login.urls')),
+	path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
